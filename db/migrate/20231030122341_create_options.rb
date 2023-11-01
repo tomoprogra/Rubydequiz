848762526b772f7ceb4ef2994c1f0a3ef38f5111
tomoprogra <anyonewww@outlook.jp>
2023-11-01 19:04:57 +0900

    commit

--- db/migrate/20231030122341_create_options.rb
+++ db/migrate/20231030122341_create_options.rb
@@ -1,10 +1,9 @@
 class CreateOptions < ActiveRecord::Migration[7.1]
   def change
-    create_table :options do |t|
+    create_table :options, id: false do |t|
       t.string :content, null: false
       t.boolean :correct, default: false
       t.references :question, null: false, foreign_key: true
-      t.integer :id, primary_key: true
       t.timestamps
     end
     add_index :options, :content
