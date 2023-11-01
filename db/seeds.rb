1335997d6171dbb5e6f50073a1f092b6c4136b21
tomoprogra <anyonewww@outlook.jp>
2023-11-01 22:10:53 +0900

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Option.delete_all
-Question.delete_all
 questions_data = [
   # 初級
   {
@@ -180,9 +178,13 @@ questions_data = [
 ]
 
 # データの保存
+Option.delete_all
+Question.delete_all
+
 questions_data.each do |data|
-    question = Question.create(content: data[:content], explanation: data[:explanation], level: data[:level])
-    data[:options].each do |option_data|
-      question.options.create(option_data)
-    end
+  question = Question.new(content: data[:content], explanation: data[:explanation], level: data[:level])
+  data[:options].each do |option_data|
+    question.options.build(option_data)
+  end
+  question.save!
 end
