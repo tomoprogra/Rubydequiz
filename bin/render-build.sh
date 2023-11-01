fe8804c012f0a723fa0adc3218ddb167cbedf460
tomoprogra <anyonewww@outlook.jp>
2023-11-01 21:58:06 +0900

    commit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-#bundle exec rake db:migrate
+bundle exec rake db:drop:_unsafe
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
