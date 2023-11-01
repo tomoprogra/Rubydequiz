a6c36127cbf5767b673aab132f67642c8adf79db
tomoprogra <anyonewww@outlook.jp>
2023-11-01 22:27:43 +0900

    commit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,17 @@
 # exit on error
 set -o errexit
 
-
+# Install gems
 bundle install
+
+# Precompile assets
 bundle exec rake assets:precompile
+
+# Clean old assets
 bundle exec rake assets:clean
+
+# Reset the database (drop, create, migrate)
+bundle exec rake db:drop db:create db:migrate
+
+# Load seed data
 bundle exec rake db:seed
