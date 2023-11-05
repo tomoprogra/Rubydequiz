d10655da6b7ec4ccbe5f52d157f40de10c1e4ebd
tomoprogra <anyonewww@outlook.jp>
2023-11-05 14:47:52 +0900

    commit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
+# Install gems
+bundle install
+
+# Precompile assets
+bundle exec rake assets:precompile
+
+# Clean old assets
+bundle exec rake assets:clean
